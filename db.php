67b2a8fac50a2ced6d1a23c66f10dd0c7793a4f6
Alvaro Romero Guillén <u201316041@upc.edu.pe>
2016-11-18 19:18:59 -0600

    cambios en BD

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
 	$servername = "us-cdbr-iron-east-04.cleardb.net";
-	$username = "b207514370152f";
-	$password = "4cbd8a50";
-	$dbname = "ad_a261e53a087dc9c";
+	$username = "bdf2d2dca7d3a1;
+	$password = "88b74944";
+	$dbname = "ad_f9d396f9a45be96";
 	// Create connection
 	$conn = mysqli_connect($servername, $username, $password, $dbname);
 	// Check connection
